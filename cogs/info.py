116feaf993bfe3e80da8bee2743d41f86df2afdc
itskatt <raph10@mail.com>
2019-06-25 11:19:26 +0200

    Change the help commad

--- cogs/info.py
+++ cogs/info.py
@@ -2,12 +2,45 @@ import discord
 from discord.ext import commands
 
 
+class HelpCommand(commands.MinimalHelpCommand):
+    def get_command_signature(self, command):
+        parent = command.full_parent_name
+        if len(command.aliases) > 0:
+            aliases = '|'.join(command.aliases)
+            fmt = f'[{command.name}|{aliases}]'
+            if parent:
+                fmt = f'{parent} {fmt}'
+            alias = fmt
+        else:
+            alias = command.name if not parent else f'{parent} {command.name}'
+        return f'{alias} {command.signature}'
+
+    def add_command_formatting(self, command):
+        if command.description:
+            self.paginator.add_line(command.description, empty=True)
+
+        signature = self.get_command_signature(command)
+        self.paginator.add_line(signature, empty=True)
+
+        if command.help:
+            try:
+                self.paginator.add_line(command.help, empty=True)
+            except RuntimeError:
+                for line in command.help.splitlines():
+                    self.paginator.add_line(line)
+                self.paginator.add_line()
+
+
 class Info(commands.Cog):
     # TODO: cog help string
     """
     """
+
     def __init__(self, bot):
         self.bot = bot
+        self._original_help_command = bot.help_command
+        bot.help_command = HelpCommand()
+        bot.help_command.cog = self
 
     @commands.command(aliases=["info"])
     async def about(self, ctx):
@@ -16,6 +49,9 @@ class Info(commands.Cog):
         """
         # basic info for now
         await ctx.send(self.bot.description)
+
+    def cog_unload(self):
+        self.bot.help_command = self._original_help_command
 
 
 def setup(bot):
